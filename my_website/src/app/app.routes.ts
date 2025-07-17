84f8dcfac038647936a7ec7e10ee72b6815c2ff8
dhivagar <dhivagarg18@gmail.com>
2025-07-18 00:19:46 +0530

    website design

--- my_website/src/app/app.routes.ts
+++ my_website/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Work } from './work/work';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {path :'', component: App},
+     {path :'work', component: Work}
+];
--- my_website/src/app/app.ts
+++ my_website/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'my_website';
+  constructor(public router:Router){}
+
+  mywor(){
+    this.router.navigate(['/work']);
+  }
 }
