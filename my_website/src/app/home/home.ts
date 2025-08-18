41b89946b4195300a98cef6cf84665bffd964c06
dhivagar <dhivagarg18@gmail.com>
2025-08-18 23:50:23 +0530

    Added animation for text content

--- my_website/src/app/home/home.ts
+++ my_website/src/app/home/home.ts
@@ -1,23 +1,57 @@
-import { Component, AfterViewInit, ElementRef, ViewChildren, QueryList  } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild , QueryList  } from '@angular/core';
 import { Router } from '@angular/router';
 import { trigger, transition, style, animate} from '@angular/animations';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
      selector: 'app-home',
-     imports: [],
+     imports: [MatTooltipModule],
      templateUrl: './home.html',
      styleUrl: './home.css',
      animations: [
-          trigger('fadeIn', [
-               transition(':enter', [ // enter means when element is added to the DOM
-               style({ opacity: 0 }),
-               animate('800ms ease-out', style({ opacity: 1 }))
-               ])
+     trigger('fadeIn', [
+          transition(':enter', [
+          style({ opacity: 0 }),
+          animate('1s ease-in', style({ opacity: 1 }))
           ])
-     ]
+     ]),
+     trigger('fadeSlideIn', [
+          transition(':enter', [ // when element is inserted (e.g. via *ngIf)
+          style({ opacity: 0, transform: 'translateY(30px)' }),
+          animate('800ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+          ]),
+          transition(':leave', [ // optional: when element is removed
+          animate('600ms ease-in', style({ opacity: 0, transform: 'translateY(30px)' }))
+          ])
+     ])
 
+     ]
 })
-export class Home {
+export class Home implements AfterViewInit{
+     @ViewChild('fadeElement') fadeElement!: ElementRef;
+
+     ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('show'); // add animation class
+            observer.unobserve(entry.target);   // run only once
+          }
+        });
+      },
+
+      { threshold: 0.2 } // trigger when 20% visible
+    );
+
+    observer.observe(this.fadeElement.nativeElement);
+
+
+  }
+     show = true;
+     toggle() { this.show = !this.show; }
+
+     currentYear: number = new Date().getFullYear();
      constructor(public router:Router){}
      mywor(){
           this.router.navigate(['/work']);
@@ -25,6 +59,7 @@ export class Home {
      about(){
           this.router.navigate(['/about']);
      }
+
      aichat(){
           this.router.navigate(['/aichat']);
      }  
